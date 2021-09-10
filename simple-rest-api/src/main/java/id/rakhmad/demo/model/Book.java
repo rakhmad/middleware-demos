464208a6429ef8632d78f66b61738c718003de10
Rakhmad Azhari <razhari@redhat.com>
2021-09-10 14:19:43 +0700

    Add custom query method.

--- simple-rest-api/src/main/java/id/rakhmad/demo/model/Book.java
+++ simple-rest-api/src/main/java/id/rakhmad/demo/model/Book.java
@@ -1,5 +1,7 @@
 package id.rakhmad.demo.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
@@ -10,4 +12,8 @@ public class Book extends PanacheEntity {
     public String author;
     public String isbn_code;
     public String publisher;
+
+    public static List<Book> findByPublisher(String publisher) {
+        return find("publisher", publisher).list();
+    }
 }
