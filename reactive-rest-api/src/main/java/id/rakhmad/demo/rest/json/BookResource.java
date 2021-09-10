1801874f687b0a1e279de5ca0ffb426f062e039f
Rakhmad Azhari <razhari@redhat.com>
2021-09-10 16:13:09 +0700

    Reactive REST API project.

---
+++ reactive-rest-api/src/main/java/id/rakhmad/demo/rest/json/BookResource.java
@@ -0,0 +1,16 @@
+package id.rakhmad.demo.rest.json;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/books")
+public class BookResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
