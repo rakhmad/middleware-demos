f9e30aaa67a3f0fc83dabc5d31ce258a867fac1f
Rakhmad Azhari <razhari@redhat.com>
2021-09-10 14:05:45 +0700

    Change to single quote.

--- simple-rest-api/src/main/resources/import.sql
+++ simple-rest-api/src/main/resources/import.sql
@@ -1,18 +1,18 @@
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "Effective Java", "Joshua Bloch", "978-0134685991", "‎Addison-Wesley Professional"); 
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "Clean Code: A Handbook of Agile Software Craftsmanship", "Robert C. Martin",
-                " 978-0132350884", "Pearson");
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "The Pragmatic Programmer: Your Journey To Mastery", "David Thomas", " 978-0135957059",
-                "‎Addison-Wesley Professional"); 
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "Design Patterns: Elements of Reusable Object-Oriented Software", "Erich Gamma, et. al.",
-                " 978-0201633610", "‎Addison-Wesley Professional");
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "Refactoring: Improving the Design of Existing Code (2nd Edition) ", "Martin Fowler",
-                " 978-0134757599", "‎Addison-Wesley Professional");
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "Working Effectively with Legacy Code", "Michael Fethers", " 978-0131177055", "‎Pearson"); 
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "Test Driven Development: By Example", "Kent Beck", " 978-0321146533",
-                "‎ Addison-Wesley Professional");
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "Extreme Programming Explained: Embrace Change, 2nd Edition", "Kent Beck", "978-0321278654",
-                "‎Addison-Wesley Professional"); 
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "Patterns of Enterprise Application Architecture", "Martin Fowler", "978-0321127426",
-                "‎Addison-Wesley Professional");  
-INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), "Domain-Driven Design: Tackling Complexity in the Heart of Software", "Eric Evans",
-                "978-0321125217", "‎Addison-Wesley Professional"); 
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'Effective Java', 'Joshua Bloch', '978-0134685991', '‎Addison-Wesley Professional'); 
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'Clean Code: A Handbook of Agile Software Craftsmanship', 'Robert C. Martin',
+                ' 978-0132350884', 'Pearson');
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'The Pragmatic Programmer: Your Journey To Mastery', 'David Thomas', ' 978-0135957059',
+                '‎Addison-Wesley Professional'); 
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'Design Patterns: Elements of Reusable Object-Oriented Software', 'Erich Gamma, et. al.',
+                ' 978-0201633610', '‎Addison-Wesley Professional');
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'Refactoring: Improving the Design of Existing Code (2nd Edition) ', 'Martin Fowler',
+                ' 978-0134757599', '‎Addison-Wesley Professional');
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'Working Effectively with Legacy Code', 'Michael Fethers', ' 978-0131177055', '‎Pearson'); 
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'Test Driven Development: By Example', 'Kent Beck', ' 978-0321146533',
+                '‎ Addison-Wesley Professional');
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'Extreme Programming Explained: Embrace Change, 2nd Edition', 'Kent Beck', '978-0321278654',
+                '‎Addison-Wesley Professional'); 
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'Patterns of Enterprise Application Architecture', 'Martin Fowler', '978-0321127426',
+                '‎Addison-Wesley Professional');  
+INSERT INTO Book(id, title, author, isbn_code, publisher) VALUES (nextval('hibernate_sequence'), 'Domain-Driven Design: Tackling Complexity in the Heart of Software', 'Eric Evans',
+                '978-0321125217', '‎Addison-Wesley Professional'); 
