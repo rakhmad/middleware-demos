060903ccb7a04d1c7e84115f045bd198f930e81a
Rakhmad Azhari <razhari@redhat.com>
2021-09-10 14:19:21 +0700

    Add queryparam.

--- simple-rest-api/src/main/java/id/rakhmad/demo/rest/json/BookResource.java
+++ simple-rest-api/src/main/java/id/rakhmad/demo/rest/json/BookResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -19,7 +20,10 @@ public class BookResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Book> books() {
+    public List<Book> books(@QueryParam("publisher") String publisher) {
+        if (publisher != null) {
+            return Book.findByPublisher(publisher);
+        }
         return Book.listAll();
     }
 
