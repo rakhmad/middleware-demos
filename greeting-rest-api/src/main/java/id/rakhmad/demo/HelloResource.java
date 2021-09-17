506a10b96527fb1865f83db8c538ff821c673469
Rakhmad Azhari <razhari@redhat.com>
2021-09-17 17:12:19 +0700

    Adding configuration.

--- greeting-rest-api/src/main/java/id/rakhmad/demo/HelloResource.java
+++ greeting-rest-api/src/main/java/id/rakhmad/demo/HelloResource.java
@@ -1,16 +1,29 @@
 package id.rakhmad.demo;
 
+import java.util.Optional;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello")
 public class HelloResource {
+
+    @ConfigProperty(name = "greeting.message")
+    String message;
+
+    @ConfigProperty(name = "greeting.suffix", defaultValue = "!")
+    String suffix;
+
+    @ConfigProperty(name = "greeting.name")
+    Optional<String> name;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return message + " " + name.orElse("world") + suffix;
     }
 }
