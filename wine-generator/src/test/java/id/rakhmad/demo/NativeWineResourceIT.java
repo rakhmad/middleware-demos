65592c776446eec0a99e9a61046a0ace954151f9
Rakhmad Azhari <razhari@redhat.com>
2021-09-18 14:17:30 +0700

    Initial commit for wine-generator.

---
+++ wine-generator/src/test/java/id/rakhmad/demo/NativeWineResourceIT.java
@@ -0,0 +1,9 @@
+package id.rakhmad.demo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeWineResourceIT extends WineResourceTest {
+
+    // Execute the same tests but in native mode.
+}
