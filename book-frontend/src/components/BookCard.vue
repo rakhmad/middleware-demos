9fc37f26901370491c10208c3626d7c3b6e867df
Rakhmad Azhari <razhari@redhat.com>
2021-09-30 14:24:48 +0700

    Component represents one book.

---
+++ book-frontend/src/components/BookCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="p-4 rounded-lg shadow-lg bg-gray-50 w-80">
+    <div class="h-48">
+      <p class="text-xl font-bold text-left text-green-800">
+        {{ book.title }}
+      </p>
+      <p class="mt-2 font-bold text-lg text-justify text-gray-700 line-clamp-4">
+        {{ book.author }}
+      </p>
+      <p class="mt-7 font-bold text-red-600">
+          {{ book.isbn_code }}
+      </p>
+      <p class="mt-1 font-light text-blue-600">
+        {{ book.publisher }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    props: {
+        book: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
