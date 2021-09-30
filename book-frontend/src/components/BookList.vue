8bdb5441305e1de37d9f04e6a06407e24c478d36
Rakhmad Azhari <razhari@redhat.com>
2021-09-30 14:24:57 +0700

    Conponent for listing all books.

---
+++ book-frontend/src/components/BookList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="grid grid-cols-1 gap-6 mt-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-items-center"
+  >
+    <BookCard 
+        v-for="book in books"
+        v-bind:book="book"
+        v-bind:key="book.id"
+    ></BookCard>
+  </div>
+</template>
+
+<script>
+import BookCard from "./BookCard.vue"
+export default {
+  props: {
+    books: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    BookCard,
+  },
+}
+</script>
