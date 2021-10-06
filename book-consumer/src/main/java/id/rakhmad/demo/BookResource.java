f50ad3d9b9905c8150d9eb9ec5de29554080bd30
Rakhmad Azhari <razhari@redhat.com>
2021-10-06 16:27:34 +0700

    Stream API for consumer.

--- book-consumer/src/main/java/id/rakhmad/demo/BookResource.java
+++ book-consumer/src/main/java/id/rakhmad/demo/BookResource.java
@@ -1,16 +1,28 @@
 package id.rakhmad.demo;
 
+import javax.inject.Inject;
+import javax.json.bind.JsonbBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.jboss.resteasy.annotations.SseElementType;
+
+import io.smallrye.mutiny.Multi;
+
 @Path("/books")
 public class BookResource {
 
+    @Inject
+    @Channel("book-in")
+    Multi<Book> books;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @SseElementType(MediaType.TEXT_PLAIN)
+    public Multi<Book> stream() {
+        return books;
     }
 }
