6448e04b6599f255ae36aa114305a0ebb9a9c2a7
Rakhmad Azhari <razhari@redhat.com>
2021-10-06 16:04:40 +0700

    Change to emitter for sending messages.

--- book-publisher/src/main/java/id/rakhmad/demo/BookGenerator.java
+++ book-publisher/src/main/java/id/rakhmad/demo/BookGenerator.java
@@ -1,33 +1,41 @@
 package id.rakhmad.demo;
 
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.json.bind.JsonbBuilder;
+import javax.inject.Inject;
 
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 
-import io.smallrye.mutiny.Multi;
+import io.quarkus.scheduler.Scheduled;
+import io.smallrye.reactive.messaging.kafka.Record;
 
 @ApplicationScoped
 public class BookGenerator {
 
-    private static final List<Book> BOOKS = Arrays
-            .asList("A Life on Our Planet", "Letter from a Stoic", "The Design of Everyday Things", "Extreme Ownership",
-                    "The Lean Startup", "The Infinite Game", "Thinking in Systems", "The Innovators",
-                    "Camel in Action 2nd Edition", "Factfulness", "Nudge", "Longitude", "Predictably Irrational",
-                    "Loonshots", "Educated", "Sapiens", "Homo Deus", "The Great Gatsby", "Nonviolent Communication",
-                    "Console Wars", "Leaders Eat Last", "Hidden Figures", "Animal Farm", "1984")
-            .stream().map(Book::of).collect(Collectors.toList());
+        private static final List<Book> BOOKS = Arrays.asList("A Life on Our Planet", "Letter from a Stoic",
+                        "The Design of Everyday Things", "Extreme Ownership", "The Lean Startup", "The Infinite Game",
+                        "Thinking in Systems", "The Innovators", "Camel in Action 2nd Edition", "Factfulness", "Nudge",
+                        "Longitude", "Predictably Irrational", "Loonshots", "Educated", "Sapiens", "Homo Deus",
+                        "The Great Gatsby", "Nonviolent Communication", "Console Wars", "Leaders Eat Last",
+                        "Hidden Figures", "Animal Farm", "1984").stream().map(Book::of).collect(Collectors.toList());
 
-    @Outgoing("book")
-    Multi<String> books() {
-        return Multi.createFrom().ticks().every(Duration.ofSeconds(1)).onOverflow().drop()
-                .map(tick -> BOOKS.get(ThreadLocalRandom.current().nextInt(0, BOOKS.size())))
-                .map(JsonbBuilder.create()::toJson);
-    }
+        @Inject
+        @Channel("book")
+        private Emitter<Record<String, Book>> emitter;
+
+        public void sendBookToKafka(Book book) {
+                emitter.send(Record.of(UUID.randomUUID().toString(), book));
+        }
+
+        @Scheduled(every = "5s")
+        public void sendRandomBookToKafka() {
+                int randomNum = ThreadLocalRandom.current().nextInt(0, BOOKS.size());
+                emitter.send(Record.of(UUID.randomUUID().toString(), BOOKS.get(randomNum)));
+        }
 }
