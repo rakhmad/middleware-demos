31308c9e3c1aa5afe7ae34b83192d7556de05728
Rakhmad Azhari <razhari@redhat.com>
2021-10-06 16:04:12 +0700

    Add new properties for Book.

--- book-publisher/src/main/java/id/rakhmad/demo/Book.java
+++ book-publisher/src/main/java/id/rakhmad/demo/Book.java
@@ -1,17 +1,21 @@
 package id.rakhmad.demo;
 
 import java.math.BigDecimal;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Book {
 
-    private String title;
+    public String title;
+    public int year;
 
-    private Book(String title) {
+    public Book(String title, int year) {
         this.title = title;
+        this.year = year;
     }
 
     public static Book of(String title) {
-        return new Book(title);
+        int year = ThreadLocalRandom.current().nextInt(1980, 2020);
+        return new Book(title, year);
     }
 
     public PricedBook withPrice(BigDecimal price) {
@@ -22,4 +26,12 @@ public class Book {
         return title;
     }
 
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book %s is published on %d.", title, year);
+    }
 }
